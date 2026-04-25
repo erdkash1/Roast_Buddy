f7275855133c22e5d15381f751fea8749ff9c4f7
Erdkash1 <erdkash1@gmail.com>
2026-04-25 15:58:07 -0500

    Add user dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
